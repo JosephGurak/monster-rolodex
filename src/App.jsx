e912581041d6d346849a03c95c0e0c9a75fb1f2d
Joseph Gurak <151475307+JosephGurak@users.noreply.github.com>
2024-12-10 23:23:56 -0500

    Initial Push
    Loaded Project from desktop to Github

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react';
+
+import CardList from './components/card-list/card-list.componet';
+import SearchBox from './components/search-box/search-box.component';
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+          monsters: [],  //initializing array 
+          searchField: ''      
+        };              
+    }
+
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')   //mounting api 
+        .then((response) => response.json())
+        .then((users) => this.setState(() => {
+          return {monsters: users}
+        }
+      ));
+    }
+
+  onSearchChange = (event) => {     // moving method outside of render component for performance increase       
+
+    const searchField = event.target.value.toLocaleLowerCase();          
+    this.setState(() => {  //call back fucntion
+      return {searchField};
+    })
+
+  }
+
+  render() {   
+    const {monsters, searchField} = this.state;  //destructuring for quality of life
+    const {onSearchChange} = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+        <div className="App">   
+          <h1 className='app-title'>Monsters Rolodex</h1>  
+                       
+          <SearchBox onChangeHandler = {onSearchChange} placeholder = {'Type Bitch'} className = "search-box" />          
+          <CardList monsters={filteredMonsters} />
+        </div>
+    );
+  }
+
+}
+
+export default App;
